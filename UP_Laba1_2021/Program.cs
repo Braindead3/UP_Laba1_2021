cec93c14a9038e82e282bf8529d1f0dc9cc4151a
Vlad <malosay21@gmail.com>
2021-01-12 08:25:56 +0300

    Добавьте файлы проекта.

---
+++ UP_Laba1_2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UP_Laba1_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
